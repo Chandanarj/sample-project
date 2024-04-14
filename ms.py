f63934b64edf5b1c4d36d4e9643e9f93bd7dbf7b
chandana naidu <chandanarj@gmail.com>
2024-04-14 16:00:33 +0530

    changes done in dhoni branch

--- ms.py
+++ ms.py
@@ -1,27 +1,12 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+# Python program to take string input and print it
 
-def find_primes(start, end):
-    primes = []
-    for num in range(start, end + 1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
+def main():
+    # Get input string from the user
+    user_input = input("Enter a string: ")
 
-start_range = int(input("Enter the start of the range: "))
-end_range = int(input("Enter the end of the range: "))
+    # Print the input string
+    print("You entered:", user_input)
 
-print("Prime numbers between", start_range, "and", end_range, "are:")
-print(find_primes(start_range, end_range))
+if __name__ == "__main__":
+    main()
 
